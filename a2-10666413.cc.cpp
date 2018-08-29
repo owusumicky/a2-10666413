eeb3b4ebedf8419bf2f7b901b1687a0952173380
michael owusu <42745016+owusumicky@users.noreply.github.com>
2018-08-29 04:16:07 -0700

    Add files via upload
    c++ source codes

---
+++ a2-10666413.cc.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main() {
+	int num, i = 0;
+	bool isprime = true;
+
+	cout <<"Enter a positive integer: \n";
+	cin >> num;
+
+    for (i = 2; i<=num/2; ++i)
+
+    if (num % i == 0)
+    {
+
+        isprime = false;
+
+        break;
+
+    }
+if (isprime)
+{
+
+
+   cout <<" This is a prime number \n \n";
+}
+
+   else
+   {
+
+
+   cout <<" This is not a prime number \n \n";
+   }
+
+
+ system("pause");
+   return 0;
+   }
+
+
+
+
